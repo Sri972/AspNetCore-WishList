f0fdc4d7db265d3cd2f28654b5df81eb805ec7b8
Sri972 <124807271+Sri972@users.noreply.github.com>
2023-02-08 11:44:21 -0600

    Update Startup.cs
    commit 2

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -33,7 +33,7 @@ namespace WishList
             }
             
             app.UseRouting();
-            app.UseEndpoints(async (endpoints) => { endpoints.MapDefaultControllerRoute(); });
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
