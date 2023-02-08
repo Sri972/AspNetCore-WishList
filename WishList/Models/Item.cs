1d4493c785b94b9fb2255f518738a53ac4cfed01
Sri972 <124807271+Sri972@users.noreply.github.com>
2023-02-08 13:46:46 -0600

    added model and data
    added model and data

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+	public class Item
+	{
+		public int Id
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		[StringLength(50)]
+		public string Description
+		{
+			get;
+			set;
+		}
+	}
+}
+
